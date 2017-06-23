90532f9d055b0f01cd8c8344f6214925b5a3b248
tran quang trung <dev.minhtri1396@Tris-Mac.local>
2017-06-23 23:45:16 +0700

    fix confict in language.swift

--- Yourself/Language.swift
+++ Yourself/Language.swift
@@ -44,12 +44,12 @@ class ButtonViews: ViewEnum {
     
     /* Choose box to swap money */
     
-    public static let NEC = MessageTitle(id: 5)
-    public static let FRA = MessageTitle(id: 6)
-    public static let LTS = MessageTitle(id: 7)
-    public static let EDU = MessageTitle(id: 8)
-    public static let PLAY = MessageTitle(id: 9)
-    public static let GIVE = MessageTitle(id: 10)
+    public static let NEC = MessageTitle(id: 6)
+    public static let FRA = MessageTitle(id: 7)
+    public static let LTS = MessageTitle(id: 8)
+    public static let EDU = MessageTitle(id: 9)
+    public static let PLAY = MessageTitle(id: 10)
+    public static let GIVE = MessageTitle(id: 11)
 }
 
 class TitleViews: ViewEnum {
@@ -57,9 +57,7 @@ class TitleViews: ViewEnum {
     public static let SPENDING_NOTE_TITLE = TitleViews(id: 1)
     public static let TIME_NOTE_TITLE = TitleViews(id: 2)
     public static let SETTINGS_TITLE = TitleViews(id: 3)
-    public static let LANGUAGE_TITLE = TitleViews(id: 4)
-    public static let DIFERENCE_SETTINGS_TITLE = TitleViews(id: 5)
-    public static let ADD_SPENDING_NOTE_TITLE = TitleViews(id: 6)
+    public static let ADD_SPENDING_NOTE_TITLE = TitleViews(id: 4)
 }
 
 class TableMenuViews: ViewEnum {
@@ -103,39 +101,22 @@ class Language {
         [
             ["Statistics", "Settings"], // OPTIONS
             ["Login Offline" ,"Login by Google" ,"Login" ,"Back", "Photo library", "Done", "Necessities", "Financial Freedom Account", "Long Term Savings", "Education", "Play", "Give"], // BUTTON
-            ["Login Offline", "Spending Notes", "Time Notes", "Settings", "Language", "More Settings", "Spending Note"], // TITLE
-            ["Statistics", "Synchrocus", "Settings", "Log out"], //TABLE_MENU
+            ["Login Offline", "Spending Notes", "Time Notes", "Settings", "Spending Note"], // TITLE
+            ["Add money", "Statistics", "Synchrocus", "Settings", "Log out"], //TABLE_MENU
             ["Notice", "Warning", "Swap money"], // MESSAGE_TITLE
             ["Email is not allowned empty!\nPlease try again!", "Your email is invalid!\nPlease check again!", "Your all box is no money", "Box which is choosed is no money", "Choose one box to swap money"], // MESSAGE
             ["Your google account(gmail)", "Add money which you need give", "Add note for giving money time" ], // PLACEHOLDER
-            ["Application exclusively for you", "This email will be used when you sync your data to Cloud (If you needed it). So, you should enter the correct email you intend to use in this app (it should be gmail account)." ] // REMINDING
-        ],
-        [
-            ["Thống kê", "Thiết lập"], // OPTIONS
-            ["Đăng nhập OFFline", "Đăng nhập bằng Google" ,"Đăng nhập" ,"Quay lại", "Kho hình ảnh", "Xong", "Cần thiết", "Đầu tư", "Tiết kiệm dài hạn", "Giáo dục", "Giải trí", "Tiêu dùng"], // BUTTON
-            ["Đăng nhập OFFLINE", "Ghi chú tài chính", "Ghi chú thời gian", "Cài đặt", "Ngôn ngữ", "Cài đặt khác", "Ghi chú tiền"], // TITLE
-            ["Thống kê", "Đồng bộ", "Cài đặt", "Đăng xuất"], //TABLE_MENU
-            ["Chú ý", "Cảnh báo", "Chuyển tiền"], // MESSAGE_TITLE
-            ["Email không được bỏ trống!\nVui lòng thử lại", "Email của bạn không tồn tại!\nVui lòng kiểm tra lại!", "Tất cả các hũ hiện không có tiền", "Hũ bạn chọn hiện không có tiền", "Chọn 1 hu3 để chuyển tiền sang"], //MESSAGE
-            ["Tài khoản google(gmail) của bạn", "Nhập số tiền cần lấy ra", "Thêm ghi chú cho lần lấy tiền này" ], // PLACEHOLDER
-            ["Ứng dụng dành riêng cho bạn", "Email này sẽ được dùng khi bạn đồng bộ dữ liệu lên Cloud (nếu bạn cần). Do đó, bạn nên nhập đúng email mà bạn dự định dùng trong ứng dụng này (Nên dùng tài khoản gmail)."  ], // REMINDING
-            ["Login Offline" ,"Login by Google" ,"Login" ,"Back", "Photo library", "Done"], // BUTTON
-            ["Login Offline", "Spending Notes", "Time Notes", "Settings"], // TITLE
-            ["Add money", "Statistics", "Synchrocus", "Settings", "Log out"], //TABLE_MENU
-            ["Notice"], // MESSAGE_TITLE
-            ["Email is not allowned empty!\nPlease try again!", "Your email is invalid!\nPlease check again!"], // MESSAGE
-            ["Your google account(gmail)"], // PLACEHOLDER
             ["Application exclusively for you", "This email will be used when you sync your data to Cloud (If you needed it). So, you should enter the correct email you intend to use in this app (it should be gmail account)." ], // REMINDING
             ["English", "Vietnamese"] // LANGUAGE
         ],
         [
             ["Thống kê", "Thiết lập"], // OPTIONS
-            ["Đăng nhập OFFline", "Đăng nhập bằng Google" ,"Đăng nhập" ,"Quay lại", "Kho hình ảnh", "Xong"], // BUTTON
-            ["Đăng nhập OFFLINE", "Ghi chú tài chính", "Ghi chú thời gian", "Cài đặt"], // TITLE
+            ["Đăng nhập OFFline", "Đăng nhập bằng Google" ,"Đăng nhập" ,"Quay lại", "Kho hình ảnh", "Xong", "Cần thiết", "Đầu tư", "Tiết kiệm dài hạn", "Giáo dục", "Giải trí", "Tiêu dùng"], // BUTTON
+            ["Đăng nhập OFFLINE", "Ghi chú tài chính", "Ghi chú thời gian", "Cài đặt", "Ghi chú tiền"], // TITLE
             ["Thêm tiền", "Thống kê", "Đồng bộ", "Cài đặt", "Đăng xuất"], //TABLE_MENU
-            ["Thông báo", "Bàn ăn", "Khu vực", "Thức ăn", "Nước uống"], // MESSAGE_TITLE
-            ["Email không được bỏ trống!\nVui lòng thử lại", "Email không hợp lệ!\nVui lòng kiểm tra lại!"], //MESSAGE
-            ["Tài khoản google(gmail) của bạn"], // PLACEHOLDER
+            ["Chú ý", "Cảnh báo", "Chuyển tiền"], // MESSAGE_TITLE
+            ["Email không được bỏ trống!\nVui lòng thử lại", "Email của bạn không tồn tại!\nVui lòng kiểm tra lại!", "Tất cả các hũ hiện không có tiền", "Hũ bạn chọn hiện không có tiền", "Chọn 1 hủ để chuyển tiền sang"], //MESSAGE
+            ["Tài khoản google(gmail) của bạn", "Nhập số tiền cần lấy ra", "Thêm ghi chú cho lần lấy tiền này" ], // PLACEHOLDER
             ["Ứng dụng dành riêng cho bạn", "Email này sẽ được dùng khi bạn đồng bộ dữ liệu lên Cloud (nếu bạn cần). Do đó, bạn nên nhập đúng email mà bạn dự định dùng trong ứng dụng này (Nên dùng tài khoản gmail)."  ], // REMINDING
             ["Tiếng Anh", "Tiếng Việt"] // LANGUAGE
         ]

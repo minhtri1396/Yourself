bfac1771ab012aac654b9c8fbd2a0a821504e420
tran quang trung <dev.minhtri1396@Tris-Mac.local>
2017-06-23 15:38:59 +0700

    Update: add spending note view and language.swift

--- Yourself/AddSpendingNoteViewController.swift
+++ Yourself/AddSpendingNoteViewController.swift
@@ -1,82 +1,178 @@
 import UIKit
 import SCLAlertView
+import BEMCheckBox
 
-class AddSpendingNoteViewController: UIViewController {
+class AddSpendingNoteViewController: UIViewController, BEMCheckBoxDelegate {
     
     // MARK: *** Local variables
     var keyboard: Keyboard?
+    var moneyOfBoxIsChoosed: Double?
+    var maxMoney: Double?
+
     
     // MARK: *** Data model
     
+    @IBOutlet weak var label_Title: UILabel!
     @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var navigationBar_TitleBar: UINavigationBar!
-    @IBOutlet weak var barButton_Back: UIBarButtonItem!
-    @IBOutlet weak var textField_AddMoney: UITextField!
+    @IBOutlet weak var textField_GivingMoney: UITextField!
     @IBOutlet weak var textField_Notes: UITextField!
     
     
+    @IBOutlet weak var button_Add: UIButton!
+    @IBOutlet weak var button_Back: UIButton!
     
     
+    @IBOutlet weak var necCheckBox: BEMCheckBox!
+    @IBOutlet weak var ffaCheckBox: BEMCheckBox!
+    @IBOutlet weak var ltssCheckBox: BEMCheckBox!
+    @IBOutlet weak var eduCheckBox: BEMCheckBox!
+    @IBOutlet weak var playCheckBox: BEMCheckBox!
+    @IBOutlet weak var giveCheckBox: BEMCheckBox!
     
+    @IBOutlet weak var necView: UIView!
+    @IBOutlet weak var ffaView: UIView!
+    @IBOutlet weak var ltssView: UIView!
+    @IBOutlet weak var eduView: UIView!
+    @IBOutlet weak var playView: UIView!
+    @IBOutlet weak var giveView: UIView!
     
+    @IBOutlet weak var necBar: UIView!
+    @IBOutlet weak var ffaBar: UIView!
+    @IBOutlet weak var ltssBar: UIView!
+    @IBOutlet weak var eduBar: UIView!
+    @IBOutlet weak var playBar: UIView!
+    @IBOutlet weak var giveBar: UIView!
     
+    @IBOutlet weak var necName: UILabel!
+    @IBOutlet weak var ffaName: UILabel!
+    @IBOutlet weak var ltssName: UILabel!
+    @IBOutlet weak var eduName: UILabel!
+    @IBOutlet weak var playName: UILabel!
+    @IBOutlet weak var giveName: UILabel!
     
-    
+    @IBOutlet weak var necMoney: UILabel!
+    @IBOutlet weak var ffaMoney: UILabel!
+    @IBOutlet weak var ltssMoney: UILabel!
+    @IBOutlet weak var eduMoney: UILabel!
+    @IBOutlet weak var playMoney: UILabel!
+    @IBOutlet weak var giveMoney: UILabel!
+        
     // MARK: *** UI events
     
     
-    @IBAction func back_Tapped(_ sender: AnyObject) {
+    @IBAction func backButton_Tapped(_ sender: AnyObject) {
         self.dismiss(animated: true, completion: nil)
     }
+   
     
-    
-    @IBAction func showMessage_Tapped(_ sender: AnyObject) {
-        let appearance = SCLAlertView.SCLAppearance(
-            kTitleFont: UIFont(name: "HelveticaNeue", size: 20)!,
-            kTextFont: UIFont(name: "HelveticaNeue", size: 14)!,
-            kButtonFont: UIFont(name: "HelveticaNeue-Bold", size: 14)!,
-            showCloseButton: false,
-            showCircularIcon: true
-        )
-        
-        let alertView = SCLAlertView(appearance: appearance)
-        
-        alertView.addButton("Necessities", target:self, selector:#selector(AddSpendingNoteViewController.Necessities_Acction))
-        alertView.addButton("Financial Freedom Account", target:self, selector:#selector(AddSpendingNoteViewController.FinancialFreedomAccount_Acction))
-        alertView.addButton("Long Term Savings", target:self, selector:#selector(AddSpendingNoteViewController.LongTermSavings_Acction))
-        alertView.addButton("Education", target:self, selector:#selector(AddSpendingNoteViewController.Education_Acction))
-        alertView.addButton("Play", target:self, selector:#selector(AddSpendingNoteViewController.Play_Acction))
-        alertView.addButton("Give", target:self, selector:#selector(AddSpendingNoteViewController.Give_Acction))
-        
-        alertView.showSuccess("Swap money", subTitle: "Choose box to send money")
+    func didTap(_ checkBox: BEMCheckBox) {
+        if (checkBox.on) {
+            var type = JARS_TYPE.EDU
+
+            if isFindingMaxMoney() {
+                switch (checkBox) {
+                case self.necCheckBox:
+                    self.necMoney.isHidden = true
+                    type = JARS_TYPE.NEC
+                    break;
+                case self.ffaCheckBox:
+                    self.ffaMoney.isHidden = true
+                    type = JARS_TYPE.FFA
+                    break;
+                case self.ltssCheckBox:
+                    self.ltssMoney.isHidden = true
+                    type = JARS_TYPE.LTSS
+                    break;
+                case self.eduCheckBox:
+                    self.eduMoney.isHidden = true
+                    type = JARS_TYPE.EDU
+                    break;
+                case self.playCheckBox:
+                    self.playMoney.isHidden = true
+                    type = JARS_TYPE.PLAY
+                    break;
+                default:
+                    self.giveMoney.isHidden = true
+                    type = JARS_TYPE.GIVE
+                    break;
+                }
+                
+                if isGettingMoneyOfBoxIsChoosed(type: type) {
+                    if Double(self.textField_GivingMoney.text!)! > moneyOfBoxIsChoosed! {
+                        show_SwapMoneyBox_ChoosingView()
+                    }
+                }
+            }
+            else {
+                checkBox.on = false
+            }
+        }
     }
+    
+    func animationDidStop(for checkBox: BEMCheckBox) {
+        if (!checkBox.on) {
+            switch (checkBox) {
+            case self.necCheckBox:
+                self.necMoney.isHidden = false
+                break;
+            case self.ffaCheckBox:
+                self.ffaMoney.isHidden = false
+                break;
+            case self.ltssCheckBox:
+                self.ltssMoney.isHidden = false
+                break;
+            case self.eduCheckBox:
+                self.eduMoney.isHidden = false
+                break;
+            case self.playCheckBox:
+                self.playMoney.isHidden = false
+                break;
+            default:
+                self.giveMoney.isHidden = false
+                break;
+            }
+        }
+    }
+
     
     
     // MARK: *** Fuction
     
-    func Necessities_Acction() {
+    /* Message box event */
+    
+    //----------- No meny massage box
+    
+    func noMoney_Acction() {
         
     }
     
-    func FinancialFreedomAccount_Acction() {
+    //----------- Choose box to replace message box
+    
+    func necessities_Acction() {
+        
+    }
+    
+    func financialFreedomAccount_Acction() {
         print("First button tapped")
     }
     
-    func LongTermSavings_Acction() {
+    func longTermSavings_Acction() {
         print("First button tapped")
     }
     
-    func Education_Acction() {
+    func education_Acction() {
         print("First button tapped")
     }
     
-    func Play_Acction() {
+    func play_Acction() {
         print("First button tapped")
     }
     
-    func Give_Acction() {
+    func give_Acction() {
         print("First button tapped")
     }
+    
+    /* Key board event */
     
     func keyboardWillShow(notification: Notification) {
         if let contentInset = keyboard?.catchEventOfKeyboard(isScroll: true, notification: notification) {
@@ -95,26 +191,20 @@ class AddSpendingNoteViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        self.view_Top1.layer.cornerRadius = self.view_Top1.frame.size.width / 2.0
-//        self.view_Body1.layer.cornerRadius = 10
-//        self.view_Top2.layer.cornerRadius = self.view_Top2.frame.size.width / 2.0
-//        self.view_Body2.layer.cornerRadius = 10
-//        self.view_Top3.layer.cornerRadius = self.view_Top3.frame.size.width / 2.0
-//        self.view_Body3.layer.cornerRadius = 10
-//        self.view_Top4.layer.cornerRadius = self.view_Top4.frame.size.width / 2.0
-//        self.view_Body4.layer.cornerRadius = 10
-//        self.view_Top5.layer.cornerRadius = self.view_Top5.frame.size.width / 2.0
-//        self.view_Body5.layer.cornerRadius = 10
-//        self.view_Top6.layer.cornerRadius = self.view_Top6.frame.size.width / 2.0
-//        self.view_Body6.layer.cornerRadius = 10
         
-        self.navigationBar_TitleBar.topItem?.title = "Thêm note"
+        configBarBox()
+        configViewBox()
+        configBorderBox()
+        configCheckBoxes()
+        configLanguge()
         
-        keyboard = Keyboard(arrTextField: [self.textField_Notes, self.textField_AddMoney])
+        keyboard = Keyboard(arrTextField: [self.textField_Notes, self.textField_GivingMoney])
         keyboard?.createDoneButton()
         
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: .UIKeyboardWillShow, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: .UIKeyboardWillHide, object: nil)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -122,4 +212,145 @@ class AddSpendingNoteViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    /* CONFIG */
+    
+    private func configAppearanceAlertView()->SCLAlertView.SCLAppearance {
+        return SCLAlertView.SCLAppearance(
+            kTitleFont: UIFont(name: "HelveticaNeue", size: 20)!,
+            kTextFont: UIFont(name: "HelveticaNeue", size: 14)!,
+            kButtonFont: UIFont(name: "HelveticaNeue-Bold", size: 14)!,
+            showCloseButton: false,
+            showCircularIcon: true
+        )
+    }
+    
+    private func configLanguge() {
+        self.button_Back.setTitle(Language.BUILDER.get(group: Group.BUTTON, view: ButtonViews.BACK_BUTTON), for: .normal)
+        self.button_Add.setTitle(Language.BUILDER.get(group: Group.BUTTON, view: ButtonViews.DONE), for: .normal)
+        self.label_Title.text = Language.BUILDER.get(group: Group.TITLE, view: TitleViews.ADD_SPENDING_NOTE_TITLE)
+        self.textField_GivingMoney.placeholder = Language.BUILDER.get(group: Group.PLACEHOLDER, view: PlaceholderViews.ADD_MONEY)
+        self.textField_Notes.placeholder = Language.BUILDER.get(group: Group.PLACEHOLDER, view: PlaceholderViews.ADD_MONEY_NOTE)
+        
+    }
+    
+    private func configBorderBox() {
+        let borderColor = UIColor(colorLiteralRed: 224/255, green: 224/255, blue: 224/255, alpha: 1).cgColor
+        self.necView.layer.borderWidth = 1
+        self.necView.layer.borderColor = borderColor
+        
+        self.ffaView.layer.borderWidth = 1
+        self.ffaView.layer.borderColor = borderColor
+        
+        self.ltssView.layer.borderWidth = 1
+        self.ltssView.layer.borderColor = borderColor
+        
+        self.eduView.layer.borderWidth = 1
+        self.eduView.layer.borderColor = borderColor
+        
+        self.playView.layer.borderWidth = 1
+        self.playView.layer.borderColor = borderColor
+        
+        self.giveView.layer.borderWidth = 1
+        self.giveView.layer.borderColor = borderColor
+    }
+    
+    private func configBarBox() {
+        self.necBar.layer.cornerRadius = 5
+        self.eduBar.layer.cornerRadius = 5
+        self.ffaBar.layer.cornerRadius = 5
+        self.ltssBar.layer.cornerRadius = 5
+        self.playBar.layer.cornerRadius = 5
+        self.giveBar.layer.cornerRadius = 5
+    }
+    
+    private func configViewBox() {
+        self.necView.layer.cornerRadius = 5
+        self.ffaView.layer.cornerRadius = 5
+        self.eduView.layer.cornerRadius = 5
+        self.ltssView.layer.cornerRadius = 5
+        self.playView.layer.cornerRadius = 5
+        self.giveView.layer.cornerRadius = 5
+    }
+
+    
+    private func configCheckBoxes() {
+        self.necCheckBox.onAnimationType = BEMAnimationType.fill
+        self.necCheckBox.offAnimationType = BEMAnimationType.fill
+        necCheckBox.delegate = self
+        
+        self.ffaCheckBox.onAnimationType = BEMAnimationType.fill
+        self.ffaCheckBox.offAnimationType = BEMAnimationType.fill
+        self.ffaCheckBox.delegate = self
+        
+        self.ltssCheckBox.onAnimationType = BEMAnimationType.fill
+        self.ltssCheckBox.offAnimationType = BEMAnimationType.fill
+        self.ltssCheckBox.delegate = self
+        
+        self.eduCheckBox.onAnimationType = BEMAnimationType.fill
+        self.eduCheckBox.offAnimationType = BEMAnimationType.fill
+        self.eduCheckBox.delegate = self
+        
+        self.playCheckBox.onAnimationType = BEMAnimationType.fill
+        self.playCheckBox.offAnimationType = BEMAnimationType.fill
+        self.playCheckBox.delegate = self
+        
+        self.giveCheckBox.onAnimationType = BEMAnimationType.fill
+        self.giveCheckBox.offAnimationType = BEMAnimationType.fill
+        self.giveCheckBox.delegate = self
+    }
+    
+    private func show_SwapMoneyBox_ChoosingView() {
+        let appearance = configAppearanceAlertView()
+        let alertView = SCLAlertView(appearance: appearance)
+        
+        alertView.addButton(Language.BUILDER.get(group: Group.BUTTON, view: ButtonViews.NEC), target:self, selector:#selector(AddSpendingNoteViewController.necessities_Acction))
+        alertView.addButton(Language.BUILDER.get(group: Group.BUTTON, view: ButtonViews.FRA), target:self,selector:#selector(AddSpendingNoteViewController.financialFreedomAccount_Acction))
+        alertView.addButton(Language.BUILDER.get(group: Group.BUTTON, view: ButtonViews.LTS), target:self, selector:#selector(AddSpendingNoteViewController.longTermSavings_Acction))
+        alertView.addButton(Language.BUILDER.get(group: Group.BUTTON, view: ButtonViews.EDU), target:self, selector:#selector(AddSpendingNoteViewController.education_Acction))
+        alertView.addButton(Language.BUILDER.get(group: Group.BUTTON, view: ButtonViews.PLAY), target:self, selector:#selector(AddSpendingNoteViewController.play_Acction))
+        alertView.addButton(Language.BUILDER.get(group: Group.BUTTON, view: ButtonViews.GIVE), target:self, selector:#selector(AddSpendingNoteViewController.give_Acction))
+        
+        alertView.showSuccess(Language.BUILDER.get(group: Group.MESSAGE_TITLE, view: MessageTitle.SWAP_MONEY), subTitle: Language.BUILDER.get(group: Group.MESSAGE, view: Message.CHOOSEBOX_SWAPMONEY))
+    }
+    
+    private func isFindingMaxMoney()->Bool {
+        
+        var typeJAR = [JARS_TYPE.EDU, JARS_TYPE.FFA, JARS_TYPE.GIVE,
+                       JARS_TYPE.LTSS, JARS_TYPE.NEC, JARS_TYPE.PLAY]
+    
+        if maxMoney == nil {
+            for i in 0..<typeJAR.count {
+                if let tmpMoney = DAOJars.BUILDER.GetJARS(with: typeJAR[i])?.money {
+                    if maxMoney == nil || maxMoney! < tmpMoney {
+                        maxMoney = tmpMoney
+                    }
+                }
+            }
+        }
+        
+        if maxMoney == nil {
+            let appearance = configAppearanceAlertView()
+            let alertView = SCLAlertView(appearance: appearance)
+            alertView.addButton("OK", target:self, selector:#selector(noMoney_Acction))
+            alertView.showError(Language.BUILDER.get(group: Group.MESSAGE_TITLE, view: MessageTitle.WARNING_MONEY), subTitle: Language.BUILDER.get(group: Group.MESSAGE, view: Message.ALLBOX_NOMONEY))
+            
+            return false
+        }
+        return true
+    }
+    
+    private func isGettingMoneyOfBoxIsChoosed(type: JARS_TYPE)->Bool {
+        if let tmp = DAOJars.BUILDER.GetJARS(with: type)?.money {
+            moneyOfBoxIsChoosed = tmp
+            return true
+        }
+        
+        let appearance = configAppearanceAlertView()
+        let alertView = SCLAlertView(appearance: appearance)
+        
+        alertView.addButton("OK", target:self, selector:#selector(noMoney_Acction))
+        alertView.showError(Language.BUILDER.get(group: Group.MESSAGE_TITLE, view: MessageTitle.WARNING_MONEY), subTitle: Language.BUILDER.get(group: Group.MESSAGE, view: Message.BOXCHOOED_NOMONEY))
+        
+        return false
+    }
 }
--- Yourself/Language.swift
+++ Yourself/Language.swift
@@ -36,6 +36,15 @@ class ButtonViews: ViewEnum {
     public static let BACK_BUTTON = ListViews(id: 3)
     public static let PHOTO_LIB_BUTTON = ListViews(id: 4)
     public static let DONE = ListViews(id: 5)
+    
+    /* Choose box to swap money */
+    
+    public static let NEC = MessageTitle(id: 5)
+    public static let FRA = MessageTitle(id: 6)
+    public static let LTS = MessageTitle(id: 7)
+    public static let EDU = MessageTitle(id: 8)
+    public static let PLAY = MessageTitle(id: 9)
+    public static let GIVE = MessageTitle(id: 10)
 }
 
 class TitleViews: ViewEnum {
@@ -45,6 +54,7 @@ class TitleViews: ViewEnum {
     public static let SETTINGS_TITLE = TitleViews(id: 3)
     public static let LANGUAGE_TITLE = TitleViews(id: 4)
     public static let DIFERENCE_SETTINGS_TITLE = TitleViews(id: 5)
+    public static let ADD_SPENDING_NOTE_TITLE = TitleViews(id: 6)
 }
 
 class TableMenuViews: ViewEnum {
@@ -57,15 +67,23 @@ class TableMenuViews: ViewEnum {
 
 class MessageTitle: ViewEnum {
     public static let NOTICE = MessageTitle(id: 0)
+    public static let WARNING_MONEY = MessageTitle(id: 1)
+    public static let SWAP_MONEY = MessageTitle(id: 2)
 }
 
 class Message: ViewEnum {
     public static let EMPTY_EMAIL = MessageTitle(id: 0)
     public static let INVALID_EMAIL = MessageTitle(id: 1)
+    public static let ALLBOX_NOMONEY = MessageTitle(id: 2)
+    public static let BOXCHOOED_NOMONEY = MessageTitle(id: 3)
+    public static let CHOOSEBOX_SWAPMONEY = MessageTitle(id: 4)
+    
 }
 
 class PlaceholderViews: ViewEnum {
     public static let GMAIL_LOGIN_OFFLINE = MessageTitle(id: 0)
+    public static let ADD_MONEY = MessageTitle(id: 1)
+    public static let ADD_MONEY_NOTE = MessageTitle(id: 2)
 }
 
 class RemindingViews: ViewEnum {
@@ -78,22 +96,22 @@ class Language {
     private static let TEXT: [[[String]]] = [
         [
             ["Statistics", "Settings"], // OPTIONS
-            ["Login Offline" ,"Login by Google" ,"Login" ,"Back", "Photo library", "Done"], // BUTTON
-            ["Login Offline", "Spending Notes", "Time Notes", "Settings", "Language", "More Settings"], // TITLE
+            ["Login Offline" ,"Login by Google" ,"Login" ,"Back", "Photo library", "Done", "Necessities", "Financial Freedom Account", "Long Term Savings", "Education", "Play", "Give"], // BUTTON
+            ["Login Offline", "Spending Notes", "Time Notes", "Settings", "Language", "More Settings", "Spending Note"], // TITLE
             ["Statistics", "Synchrocus", "Settings", "Log out"], //TABLE_MENU
-            ["Notice"], // MESSAGE_TITLE
-            ["Email is not allowned empty!\nPlease try again!", "Your email is invalid!\nPlease check again!"], // MESSAGE
-            ["Your google account(gmail)"], // PLACEHOLDER
+            ["Notice", "Warning", "Swap money"], // MESSAGE_TITLE
+            ["Email is not allowned empty!\nPlease try again!", "Your email is invalid!\nPlease check again!", "Your all box is no money", "Box which is choosed is no money", "Choose one box to swap money"], // MESSAGE
+            ["Your google account(gmail)", "Add money which you need give", "Add note for giving money time" ], // PLACEHOLDER
             ["Application exclusively for you", "This email will be used when you sync your data to Cloud (If you needed it). So, you should enter the correct email you intend to use in this app (it should be gmail account)." ] // REMINDING
         ],
         [
             ["Thống kê", "Thiết lập"], // OPTIONS
-            ["Đăng nhập OFFline", "Đăng nhập bằng Google" ,"Đăng nhập" ,"Quay lại", "Kho hình ảnh", "Xong"], // BUTTON
-            ["Đăng nhập OFFLINE", "Ghi chú tài chính", "Ghi chú thời gian", "Cài đặt", "Ngôn ngữ", "Cài đặt khác"], // TITLE
+            ["Đăng nhập OFFline", "Đăng nhập bằng Google" ,"Đăng nhập" ,"Quay lại", "Kho hình ảnh", "Xong", "Cần thiết", "Đầu tư", "Tiết kiệm dài hạn", "Giáo dục", "Giải trí", "Tiêu dùng"], // BUTTON
+            ["Đăng nhập OFFLINE", "Ghi chú tài chính", "Ghi chú thời gian", "Cài đặt", "Ngôn ngữ", "Cài đặt khác", "Ghi chú tiền"], // TITLE
             ["Thống kê", "Đồng bộ", "Cài đặt", "Đăng xuất"], //TABLE_MENU
-            ["Thông báo", "Bàn ăn", "Khu vực", "Thức ăn", "Nước uống"], // MESSAGE_TITLE
-            ["Email không được bỏ trống!\nVui lòng thử lại", "Email của bạn không tồn tại!\nVui lòng kiểm tra lại!"], //MESSAGE
-            ["Tài khoản google(gmail) của bạn"], // PLACEHOLDER
+            ["Chú ý", "Cảnh báo", "Chuyển tiền"], // MESSAGE_TITLE
+            ["Email không được bỏ trống!\nVui lòng thử lại", "Email của bạn không tồn tại!\nVui lòng kiểm tra lại!", "Tất cả các hũ hiện không có tiền", "Hũ bạn chọn hiện không có tiền", "Chọn 1 hu3 để chuyển tiền sang"], //MESSAGE
+            ["Tài khoản google(gmail) của bạn", "Nhập số tiền cần lấy ra", "Thêm ghi chú cho lần lấy tiền này" ], // PLACEHOLDER
             ["Ứng dụng dành riêng cho bạn", "Email này sẽ được dùng khi bạn đồng bộ dữ liệu lên Cloud (nếu bạn cần). Do đó, bạn nên nhập đúng email mà bạn dự định dùng trong ứng dụng này (Nên dùng tài khoản gmail)."  ] // REMINDING
         ]
     ]
